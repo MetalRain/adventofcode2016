1c942f174b6362f07e3683e2f65dd342a1669739
Otto Martikainen <martikainenotto@gmail.com>
2016-12-20 21:45:42 +0200

    First part solved

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 module Main where
 
 import Lib
+import Data.List
 import Data.Maybe
 import Data.Monoid
 
 ruleFile :: FilePath
 ruleFile = "./data/rules.txt"
 
-showRange (Range b e) = show b <> "->" <> show e
+showRange (Range b e) = show b <> " -> " <> show e
 
 main :: IO ()
-main = do 
+main = do
   ranges <- readRanges ruleFile
-  print $ map (showRange . fromJust) $ filter isJust ranges
+  mapM_ putStrLn $ map showRange $ take 30 $ extendSelf $ map fromJust $ filter isJust ranges
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,27 @@
 module Lib
     ( readRanges
     , Range(..)
+    , extendSelf
     ) where
 
+import Data.List
 import Data.List.Split
 import Data.Maybe
 import Text.Read
 
-data Range = Range Int Int 
+data Range = Range Int Int deriving Show 
 
+instance Eq Range where
+  (Range s1 e1) == (Range s2 e2) = (s1 == s2 && e1 == e2)
+
+instance Ord Range where
+  (Range s1 _) < (Range s2 _) = s1 < s2
+  (Range s1 _) <= (Range s2 _) = s1 <= s2
+
+splitLines :: String -> [String]
 splitLines = splitOn "\n"
+
+splitEnds :: String -> [String]
 splitEnds = splitOn "-"
 
 readRanges :: FilePath -> IO [Maybe Range]
@@ -17,20 +29,32 @@ readRanges p = do
   ruleString <- readFile p
   return $ map parseRange $ splitLines ruleString
 
+buildRange :: Int -> Int -> Range
+buildRange s e = Range (minimum [s, e]) (maximum [s, e])
+
 parseRange :: String -> Maybe Range
 parseRange s 
-  | all isJust [start, end] = Just (Range (fromJust start) (fromJust end))
+  | all isJust [start, end] = Just (buildRange (fromJust start) (fromJust end))
   | otherwise = Nothing where
-      parts = splitEnds s :: [String]
-      start = readMaybe (minimum parts) :: Maybe Int
-      end   = readMaybe (maximum parts) :: Maybe Int
+     parts = splitEnds s :: [String]
+     start = readMaybe (minimum parts) :: Maybe Int
+     end   = readMaybe (maximum parts) :: Maybe Int 
 
---extendRanges :: Range -> [Range] -> [Range]
---extendRanges r rs = 
+squash :: Range -> Range -> Range
+squash extender orig
+  | next_by || cutting || inside  = buildRange (minimum [s1, s2]) (maximum [e1, e2])
+  | otherwise                     = orig where
+     (Range s1 e1) = extender
+     (Range s2 e2) = orig
+     next_by       = (e2 + 1 == s1) || (e1 + 1 == s2)
+     cutting       = (s1 <= e2 && s2 <= e1) || (s2 <= e1 && s1 <= e2)
+     inside        = (s1 <= s2 && e2 <= e1) || (s2 <= s1 && e1 <= e2)
 
---joinRanges :: Range -> [Range] -> [Range]
---joinRanges r rs = 
---  | [] == rs = [r]
+squashWith :: Range -> [Range] -> [Range]
+squashWith = map . squash
 
-
-
+extendSelf :: [Range] -> [Range]
+extendSelf xs
+  | xs == extended  = extended 
+  | otherwise       = extendSelf extended where
+     extended = nub $ sort $ foldr squashWith xs xs
